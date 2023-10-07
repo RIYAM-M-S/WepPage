7bdc344c7f6e4e62d4254849c903fbf03a4fb30e
Riyam <88596299+RIYAM-M-S@users.noreply.github.com>
2023-10-08 00:54:35 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# registration_WebPage
+the project is  about securing a registration web page that was filled with vulnerabilities 
+we secured it by the following changes/
+- hashing the password.
+- changing the value to POST of the HTML form's METHOD attribute to hide sensitive/personal information from the third party.
+- adding a Strong password policy
+- preventing SQL injection by utilizing parameterized database queries with bound to handle escaping untrusted user input.
+  
---
+++ index.js
@@ -0,0 +1,59 @@
+const bcrypt = require('bcrypt'); 
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const salt = bcrypt.genSaltSync(10) ;
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+const database = require("./database/database.js"); 
+
+
+app.use('/', express.static(path.join(__dirname, 'public', 'login')));
+app.use('/signup', express.static(path.join(__dirname, 'public', 'signup')));
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword1 = bcrypt.hashSync(password, salt);
+  const user = {
+    username : username,
+    password : hashedPassword1
+  }
+  database.authenticate(user)
+  .then((result) => {
+    if(result.length > 0){
+      res.json(result);
+    }
+    else{
+      res.redirect('/?error=true');
+    }
+  }
+  )
+});
+
+app.get('/submitSignup', (req, res) => {
+  const { username, password } = req.query;
+  const hashedPassword2 = bcrypt.hashSync(password, salt);
+  const user = {
+    username : username,
+    password : hashedPassword2
+  }
+  database.signup(user)
+  .then((result) => {
+    if(result){
+      res.json("user created! please login");
+    }
+    else{
+      res.redirect('/signup?error=true');
+    }
+  }
+  )
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
